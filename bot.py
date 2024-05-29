d7898c2dbd69df0130b7e799bef7442b57ea69b8
pure-life-git <roscoegoodman02@gmail.com>
2024-05-28 19:37:00 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,9 +64,9 @@ async def on_voice_state_update(member: discord.Member, before: discord.VoiceSta
         return
     
     if member.guild.get_role(1241950590725128272) in member.roles and (before.self_stream == False and after.self_stream == True):
-        sleeper_agent = random.randint(30,500)
+        sleeper_agent = random.randint(30,300)
         print(f"Booting {member.name} in {sleeper_agent} seconds")
-        await time.sleep(sleeper_agent)
+        await asyncio.sleep(sleeper_agent)
         await member.move_to(member.guild.get_channel(1241961286774952007))
         await member.move_to(member.guild.get_channel(644075079558365188))
         await member.send(content="Certified Stafford moment", tts=True)
