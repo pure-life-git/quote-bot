4655f27dbf93786b7630197e0c2a0c8dddeb893f
Theo Goodman <theogoodman@Theos-MacBook-Air.local>
2025-07-08 20:39:59 -0700

    haha so funny testing

--- quote-bot/bot.py
+++ quote-bot/bot.py
@@ -166,7 +166,7 @@ async def on_message(msg: discord.Message):
     print(msg)
     daniel = bot.get_user(149337676343017472)
     theo = bot.get_user(288710564367171595)
-    if len(msg.attachments) != 0 and msg.attachment[1].content_type.startswith("image"):
+    if len(msg.attachments) != 0 and msg.attachments[0].startswith("image"):
         await msg.reply(content="Haha this is so funny am i right guys?")
 
 
